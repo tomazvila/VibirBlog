faa942dba8bd4cd525f3c3649183c055056b0ce8
tomazvila <tomazvila@outlook.com>
2021-12-29 19:24:47 +0200

    Remove old project

--- blog/Web/Routes.hs
+++
@@ -1,9 +0,0 @@
-module Web.Routes where
-import IHP.RouterPrelude
-import Generated.Types
-import Web.Types
-
--- Generator Marker
-instance AutoRoute StaticController
-instance AutoRoute PostsController
-
