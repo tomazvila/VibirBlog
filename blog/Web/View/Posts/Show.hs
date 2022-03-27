4411dc2e462134744a4354ff5b71c60b038e4eb8
tomazvila <tomazvila@outlook.com>
2022-03-28 00:10:40 +0300

    Render posts as markdown

--- blog/Web/View/Posts/Show.hs
+++ blog/Web/View/Posts/Show.hs
@@ -1,17 +1,27 @@
 module Web.View.Posts.Show where
 import Web.View.Prelude
+import qualified Text.MMark as MMark
 
 data ShowView = ShowView { post :: Post }
 
 instance View ShowView where
     html ShowView { .. } = [hsx|
-        {breadcrumb}
-        <h1>Show Post</h1>
-        <p>{post}</p>
-
+       <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href={PostsAction}>Posts</a></li>
+                <li class="breadcrumb-item active">Show Post</li>
+            </ol>
+        </nav>
+        <h1>{get #title post} - title of post</h1>
+        <div>{get #body post |> renderMarkdown}</div>
     |]
         where
             breadcrumb = renderBreadcrumb
                             [ breadcrumbLink "Posts" PostsAction
                             , breadcrumbText "Show Post"
                             ]
+
+renderMarkdown text =  -- text |> MMark.parse "" -- empty string is the name of a markdown file in this case it doesn't exist so we pass empty string
+  case text |> MMark.parse "" of
+        Left error -> "Something went wrong"
+        Right markdown -> MMark.render markdown |> tshow |> preEscapedToHtml
